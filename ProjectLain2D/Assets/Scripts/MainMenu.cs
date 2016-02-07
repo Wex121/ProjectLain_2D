9595ac7ba72ba2f5c9caabbece93b4365fd0c10f
Wex <jtgowan@yahoo.com>
2016-02-07 15:35:22 -0500

    Added Player Controller, menu

---
+++ ProjectLain2D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+    public string scene;
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
